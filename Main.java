21676b42fc61d5d11ed0d5c05c9537e8de19c2e2
Jack <hui2@pdx.edu>
2019-03-10 12:19:38 -0700

    Hangman Game, very bad time complexity and implementation

--- Main.java
+++ Main.java
@@ -1,7 +1,47 @@
-public class Main {
-
-    static void main(String [] args)
+import java.util.Scanner;
+public class Main extends Resource
+{
+    public static void main(String [] args)
     {
-        System.out.println("Testing");
+        boolean check = true;
+        do {
+            Main s = new Main();
+            char[] characters = new char[s.word.length()];
+            char[] test = new char[s.word.length()];
+            for(int i = 0;i<s.word.length();++i)
+            {
+                test[i] = s.word.charAt(i);
+                characters[i] = '_';
+            }
+            String checking = "";
+            do {
+                for(int i = 0;i<characters.length;++i) {
+                    System.out.print(characters[i] + " ");
+                }
+                System.out.print("Enter a character: ");
+                Scanner myObj = new Scanner(System.in);
+                String answer = myObj.next();
+                char testing = answer.charAt(0);
+                for(int j = 0; j<characters.length; ++j)
+                {
+                   if(Character.toLowerCase(testing) == Character.toLowerCase(test[j]))
+                   {
+                       characters[j] = testing;
+                   }
+                }
+                for (int x = 0; x < characters.length; ++x) {
+                    checking = checking + characters[x];
+                }
+                checking.trim();
+            }while(!checking.contentEquals(s.word));
+
+            System.out.println("Another game? (y/n)");
+            Scanner myObj = new Scanner(System.in);
+            String answer = myObj.next();
+            if(answer.contentEquals("y"))
+                check = true;
+            else
+                check = false;
+        }while(check);
     }
 }
--- Resource.java
+++ Resource.java
@@ -1,6 +1,27 @@
+import java.util.Random;
+import java.io.*;
+
 public class Resource {
+    String word = new String();
+    String[]alist = new String[8];
     Resource ()
     {
-
+        Random rand = new Random();
+        //head = rand.nextInt(50);
+        //temp = rand.nextInt(50);
+        File file = new File("/Users/Jack/Documents/CodingPrac/Project1/src/lib");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+            int counter = 0;
+            while ((st = br.readLine()) != null){
+                alist[counter] = st;
+                ++counter;
+            }
+            int num = rand.nextInt(7);
+            word = alist[num];
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
